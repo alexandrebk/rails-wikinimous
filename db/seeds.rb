11282d12355fbdce2168588e07905c288d00636a
Alexandre Bouvier <alexandre.b2506@gmail.com>
2021-07-03 16:18:09 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+10.times do
+  article = Article.new(
+    title:   Faker::Book.title,
+    content: Faker::ChuckNorris.fact
+  )
+
+  article.save
+end
+
+puts "#{Article.count} articles created"
